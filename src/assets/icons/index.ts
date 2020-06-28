767f34a48c1ea543a4622a1da9bbbd39be574dec
Evan K <ekerik220@gmail.com>
2020-06-28 16:03:06 +0900

    Added SVG icon components

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { MagnifyingGlassIcon } from "./MagnifyingGlassIcon";
+export { PeopleIcon } from "./PeopleIcon";
+export { SpeechBubbleIcon } from "./SpeechBubbleIcon";
+export { TwitterBird } from "./TwitterBird";
