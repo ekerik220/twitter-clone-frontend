7cc5ed21c5ba76767a290acbfd0aca3663d3bf5a
Evan K <ekerik220@gmail.com>
2020-06-28 16:02:36 +0900

    Added to themes and global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,4 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
+* {
+    box-sizing: border-box;
+}
+
+html, body, #root {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    font-family: ${({ theme }) => theme.fontFamily}
+}
 `;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,1 +1,13 @@
-export const theme = {};
+export const theme = {
+  fontFamily:
+    'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu, "Helvetica Neue", sans-serif',
+  inputFont: "Arial",
+  colors: {
+    greyText: "#657786",
+    greyBackground: "rgb(245, 248, 250)",
+    blueMain: "rgb(29,161,242)",
+    blueSecondary: "rgb(113, 201, 248)",
+    primaryButtonHover: "rgb(26, 145, 218)",
+    outlineButtonHover: "rgba(29,161,242,0.1)",
+  },
+};
