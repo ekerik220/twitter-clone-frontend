f138b17c173baa8bc185cf3e48f4b53ba1085be7
Evan K <ekerik220@gmail.com>
2020-06-28 16:04:20 +0900

    Added a twitter style Button component

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+type PropTypes = {
+  variation?: "primary" | "outline";
+  children?: React.ReactNode;
+  className?: string;
+};
+
+export function Button(props: PropTypes) {
+  return (
+    <Wrapper variation={props.variation} className={props.className}>
+      {props.children}
+    </Wrapper>
+  );
+}
+
+type WrapperProps = {
+  variation?: string;
+};
+const Wrapper = styled.a.attrs({ role: "button" })<WrapperProps>`
+  align-items: center;
+  border-radius: 100px;
+  cursor: pointer;
+  display: flex;
+  font-size: 15px;
+  font-weight: bold;
+  justify-content: center;
+  min-height: 39px;
+  transition: background-color 0.2s;
+  user-select: none;
+
+  ${({ variation }) =>
+    variation === "outline" ? outlineStyles : primaryStyles}
+`;
+
+const primaryStyles = css`
+  background-color: ${({ theme }) => theme.colors.blueMain};
+  color: white;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.primaryButtonHover};
+  }
+`;
+
+const outlineStyles = css`
+  border: ${({ theme }) => `1px solid ${theme.colors.blueMain}`};
+  color: ${({ theme }) => theme.colors.blueMain};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.outlineButtonHover};
+  }
+`;
