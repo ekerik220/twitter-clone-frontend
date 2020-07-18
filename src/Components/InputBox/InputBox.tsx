21af3246aa01bd976a072cba9c0d7091e9d04161
Evan K <ekerik220@gmail.com>
2020-07-18 09:30:06 +0900

    Updated + refactored InputBox component
     - Can pass it a value/onChange function
     - Can set error state to true/false (true makes the text and border red).
     - Fixed border clipping issues at certain window sizes.

--- src/Components/InputBox/InputBox.tsx
+++ src/Components/InputBox/InputBox.tsx
@@ -2,51 +2,75 @@ import React from "react";
 import styled from "styled-components";
 
 type PropTypes = {
-  title: string;
-  value: string;
+  title?: string;
+  value?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  error?: boolean;
   className?: string;
 };
 
-export function InputBox(props: PropTypes) {
+export function InputBox({
+  title,
+  value,
+  onChange,
+  error = false,
+  className,
+}: PropTypes) {
   return (
-    <Wrapper className={props.className}>
-      <span>{props.title}</span>
-      <input value={props.value} />
+    <Wrapper className={className} error={error}>
+      <label>
+        <Title error={error}>{title}</Title>
+        <Input error={error} value={value} onChange={onChange} />
+      </label>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div`
+type TitleProps = { error: boolean };
+const Title = styled.span<TitleProps>`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: ${({ theme, error }) =>
+    error ? theme.colors.errorRed : theme.colors.greyText};
+  margin-left: 10px;
+`;
+
+type InputProps = { error: boolean };
+const Input = styled.input<InputProps>`
+  display: block;
+  width: 100%;
+  border: none;
+  outline-style: none;
+  background-color: ${({ theme }) => theme.colors.greyBackground};
+  padding-top: 2px;
+  padding-bottom: 5px;
+  padding-left: 10px;
+  font-size: 19px;
+  font-family: ${({ theme }) => theme.inputFont};
+  line-height: 1.3;
+  border-bottom: 2px solid;
+  border-color: ${({ theme, error }) =>
+    error ? theme.colors.errorRed : "black"};
+`;
+
+type WrapperProps = { error: boolean };
+const Wrapper = styled.div<WrapperProps>`
   display: flex;
   flex-direction: column;
   background-color: ${({ theme }) => theme.colors.greyBackground};
-  padding: 5px 10px 0;
-  border-bottom: 3px solid black;
+  padding-top: 5px;
   font-size: 15px;
 
-  span {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    color: ${({ theme }) => theme.colors.greyText};
-  }
-
-  input {
-    border: none;
-    outline-style: none;
-    background-color: ${({ theme }) => theme.colors.greyBackground};
-    padding-top: 2px;
-    padding-bottom: 5px;
-    font-size: 19px;
-    font-family: ${({ theme }) => theme.inputFont};
-    line-height: 1.3;
-  }
-
   &:focus-within {
-    border-color: ${({ theme }) => theme.colors.blueMain};
+    ${Input} {
+      border-color: ${({ theme, error }) =>
+        error ? theme.colors.errorRed : theme.colors.blueMain};
+    }
 
-    span {
-      color: ${({ theme }) => theme.colors.blueMain};
+    ${Title} {
+      color: ${({ theme, error }) =>
+        error ? theme.colors.errorRed : theme.colors.blueMain};
     }
   }
 `;
