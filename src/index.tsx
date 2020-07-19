67f6f8c8b9729c79086b0068da5bd188e2add06a
Evan K <ekerik220@gmail.com>
2020-07-19 12:54:47 +0900

    Set up ApolloClient

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,24 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyles, theme } from "styles";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ThemeProvider>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ThemeProvider>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
