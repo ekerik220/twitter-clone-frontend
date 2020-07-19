4ad9947e9e4ab8b522a7bab39c4fc25449ec2ef5
Evan K <ekerik220@gmail.com>
2020-07-19 12:57:59 +0900

    Added PageOne component (page 1 of SignupModal)

---
+++ src/Components/SignupModal/PageOne.tsx
@@ -0,0 +1,241 @@
+import React, { useState, useCallback, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useLazyQuery, gql } from "@apollo/client";
+import styled from "styled-components";
+import { InputBox } from "Components/InputBox/InputBox";
+import { SelectBox } from "Components/SelectBox/SelectBox";
+import {
+  nameChanged,
+  emailChanged,
+  dayChanged,
+  monthChanged,
+  yearChanged,
+  currentPageIsInvalid,
+  currentPageIsValid,
+} from "redux/slices/signupSlice";
+import _ from "lodash";
+import * as EmailValidator from "email-validator";
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+// [1..31]
+const days = Array.from(Array(31), (_, i) => (i + 1).toString());
+
+// array containing all years from current year to 100 years back
+const currYear = new Date().getFullYear();
+const years = Array.from(Array(101), (_, i) =>
+  (i + currYear - 100).toString()
+).reverse();
+
+const EMAIL_TAKEN = gql`
+  query EmailTaken($email: String!) {
+    emailTaken(email: $email)
+  }
+`;
+
+/*********************************
+ *  Page one of the sign up modal
+ *********************************/
+export function PageOne() {
+  const dispatch = useDispatch();
+  const [emailTaken, { data, variables }] = useLazyQuery(EMAIL_TAKEN);
+
+  // * Local state
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [emailTakenError, setEmailTakenError] = useState(false);
+
+  // * Bring in form state from Redux
+  const { name, email, day, month, year } = useSelector(
+    (state: RootState) => state.signup.userInfo
+  );
+
+  // * Update redux state as the name input field changes + error checking.
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    value.length === 0 ? setNameError(true) : setNameError(false);
+    dispatch(nameChanged(value));
+  };
+
+  // * Debounced error checking on the email field to be used with handleEmailChange
+  const debouncedEmailErrorCheck = useCallback(
+    _.debounce((email: string) => {
+      // Invalid email
+      if (!EmailValidator.validate(email)) return setEmailError(true);
+
+      // Query server to see if it's taken...
+      // The actual error checking will be handled by a useEffect
+      emailTaken({ variables: { email } });
+    }, 700),
+    []
+  );
+
+  // * Watch the results of emailTaken query and update the error state
+  useEffect(() => {
+    // Since there's a lag between requesting the check and getting
+    // the results we have to also check if the email we requested
+    // a check for is still the email that is currently inputted
+    if (data?.emailTaken === true && variables?.email === email)
+      setEmailTakenError(true);
+  }, [data, variables, email]);
+
+  // * Update redux state as the email input field changes + error checking (debounced).
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    // reset both error types to false as the user types
+    setEmailError(false);
+    setEmailTakenError(false);
+
+    // Check for errors (valid email + not taken) debounced
+    debouncedEmailErrorCheck(value);
+
+    // Update the field (regardless of errors)
+    dispatch(emailChanged(value));
+  };
+
+  // * Check if all the fields on this page of the form are valid
+  // * and tell redux about it
+  useEffect(() => {
+    const fieldsNonEmpty = name && email && day && month && year;
+    const noErrors = !nameError && !emailError && !emailTakenError;
+
+    if (fieldsNonEmpty && noErrors) dispatch(currentPageIsValid());
+    else dispatch(currentPageIsInvalid());
+  }, [
+    name,
+    email,
+    day,
+    month,
+    year,
+    nameError,
+    emailError,
+    emailTakenError,
+    dispatch,
+  ]);
+
+  return (
+    <Container>
+      <Title>Create your account</Title>
+      <InputBox
+        title="Name"
+        value={name}
+        onChange={handleNameChange}
+        maxLength={50}
+        error={nameError}
+      />
+      <Details>
+        {nameError && <ErrorText>What's your name?</ErrorText>}
+        <Counter>{`${name.length}/50`}</Counter>
+      </Details>
+      <InputBox
+        title="Email"
+        value={email}
+        onChange={handleEmailChange}
+        maxLength={80}
+        error={emailError || emailTakenError}
+      />
+      <Details>
+        {emailError && <ErrorText>Please enter a valid email.</ErrorText>}
+        {emailTakenError && (
+          <ErrorText>Email has already been taken.</ErrorText>
+        )}
+      </Details>
+      <Subtitle>Date of birth</Subtitle>
+      <Subtext>
+        This will not be shown publicly. Confirm your own age, even if this
+        account is for a business, a pet, or something else.
+      </Subtext>
+      <DateWrapper>
+        <MonthSelect
+          title="Month"
+          options={months}
+          value={month}
+          onChange={(e) => dispatch(monthChanged(e.target.value))}
+        />
+        <DaySelect
+          title="Day"
+          options={days}
+          value={day}
+          onChange={(e) => dispatch(dayChanged(e.target.value))}
+        />
+        <YearSelect
+          title="Year"
+          options={years}
+          value={year}
+          onChange={(e) => dispatch(yearChanged(e.target.value))}
+        />
+      </DateWrapper>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 525px;
+  margin: auto;
+`;
+
+const Title = styled.h2`
+  font-size: 23px;
+`;
+
+const Details = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.colors.greyText};
+  padding: 4px 0;
+`;
+
+const Subtitle = styled.h4`
+  margin-top: 20px;
+  margin-bottom: 0;
+`;
+
+const Subtext = styled.span`
+  font-size: 16px;
+  color: ${({ theme }) => theme.colors.greyText};
+`;
+
+const Counter = styled(Subtext)`
+  grid-column-start: 2;
+  justify-self: flex-end;
+`;
+
+const ErrorText = styled(Subtext)`
+  color: ${({ theme }) => theme.colors.errorRed};
+`;
+
+const DateWrapper = styled.div`
+  display: flex;
+  margin: 20px 0;
+`;
+
+const MonthSelect = styled(SelectBox)`
+  margin-right: 10px;
+  width: 45%;
+`;
+
+const DaySelect = styled(SelectBox)`
+  margin-right: 10px;
+  width: 25%;
+`;
+
+const YearSelect = styled(SelectBox)`
+  width: 30%;
+`;
