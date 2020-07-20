91386f470b1761b5aac12a29b6bed0a3879f7908
Evan K <ekerik220@gmail.com>
2020-07-20 11:47:28 +0900

    Added LoadingIcon SVG component

---
+++ src/assets/icons/LoadingIcon.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+export function LoadingIcon(props: { className?: string }) {
+  return (
+    <svg
+      height="100%"
+      width="100%"
+      viewBox="0 0 32 32"
+      className={props.className}
+    >
+      <g>
+        <circle
+          cx="16"
+          cy="16"
+          fill="none"
+          r="14"
+          stroke-width="4"
+          style={{ stroke: "rgb(29, 161, 242)", opacity: "0.2" }}
+        ></circle>
+      </g>
+      <RotatingCircle>
+        <circle
+          cx="16"
+          cy="16"
+          fill="none"
+          r="14"
+          stroke-width="4"
+          style={{
+            stroke: "rgb(29, 161, 242)",
+            strokeDasharray: "80px",
+            strokeDashoffset: "60px",
+          }}
+        ></circle>
+      </RotatingCircle>
+    </svg>
+  );
+}
+
+const rotate = keyframes`
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
+`;
+
+const RotatingCircle = styled.g`
+  transform-origin: center;
+  animation-name: ${rotate};
+  animation-duration: 1s;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  height: 1px;
+`;
