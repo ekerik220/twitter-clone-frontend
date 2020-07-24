89c71c907345fe5ba121f3906e9c2b51bb70062e
Evan K <ekerik220@gmail.com>
2020-07-24 15:06:20 +0900

    Added imageEditor and user state reducers to the store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,8 +1,12 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import signup from "./slices/signupSlice";
+import user from "./slices/userSlice";
+import imageEditor from "./slices/imageEditorSlice";
 
 const reducer = combineReducers({
   signup,
+  user,
+  imageEditor,
 });
 
 export const store = configureStore({
