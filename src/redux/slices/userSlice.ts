305bfd044888dfa89d1c95dce8b54fe961afa0eb
Evan K <ekerik220@gmail.com>
2020-07-24 21:25:18 +0900

    Default state of token wasn't null (testing purposes)

--- src/redux/slices/userSlice.ts
+++ src/redux/slices/userSlice.ts
@@ -1,8 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 const initialState: RootState["user"] = {
-  token:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMWE0ODFlNzhhOGY1NzAxN2YwNWQ1OSIsImlhdCI6MTU5NTU1ODYxMn0.IO2uN0S5Fq4z08t2XDeIKtkCRCMVuEDICOaCre8Z8I0",
+  token: null,
   avatar:
     "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png",
 };
