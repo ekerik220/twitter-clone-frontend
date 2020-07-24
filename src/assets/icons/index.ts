c9edff30a3ac5bbbeea2efbd118b165cacb6c9a3
Evan K <ekerik220@gmail.com>
2020-07-24 13:25:56 +0900

    Updated exports in barrel file for icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,3 +3,5 @@ export { PeopleIcon } from "./PeopleIcon";
 export { SpeechBubbleIcon } from "./SpeechBubbleIcon";
 export { TwitterBird } from "./TwitterBird";
 export { DownArrow } from "./DownArrow";
+export { LoadingIcon } from "./LoadingIcon";
+export { CameraIcon } from "./CameraIcon";
