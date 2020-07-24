f72ec7edf89bfd91f368c3885080f53546a5de8b
Evan K <ekerik220@gmail.com>
2020-07-24 21:41:34 +0900

    Added the start of App with some routing and a blank Home page

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,8 +1,34 @@
 import React from "react";
-import { LoginPage } from "Components";
+import { useSelector } from "react-redux";
+import { LoginPage, SignupModal } from "Components";
+import { ImageEditor } from "./ImageEditor/ImageEditor";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./Home/Home";
 
 function App() {
-  return <LoginPage />;
+  // Redux state
+  const imageEditorOpen = useSelector(
+    (state: RootState) => state.imageEditor.open
+  );
+  const signupModalOpen = useSelector(
+    (state: RootState) => state.signup.modalOpen
+  );
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+      {/* Modals */}
+      {imageEditorOpen && <ImageEditor />}
+      {signupModalOpen && <SignupModal />}
+    </Router>
+  );
 }
 
 export default App;
