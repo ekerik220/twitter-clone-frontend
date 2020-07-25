0b12ff087b8b047358ce01ee100359ed5b447656
Evan K <ekerik220@gmail.com>
2020-07-25 14:05:20 +0900

    Set default size for loading icon to 25x25px

--- src/assets/icons/LoadingIcon.tsx
+++ src/assets/icons/LoadingIcon.tsx
@@ -4,8 +4,8 @@ import styled, { keyframes } from "styled-components";
 export function LoadingIcon(props: { className?: string }) {
   return (
     <svg
-      height="100%"
-      width="100%"
+      height="25px"
+      width="25px"
       viewBox="0 0 32 32"
       className={props.className}
     >
