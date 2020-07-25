d8e18ff1116ff6cc5d744615fd2be5528bdb4040
Evan K <ekerik220@gmail.com>
2020-07-25 15:27:02 +0900

    /home redirects to '/' if not logged in

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 export function Home() {
+  const history = useHistory();
+
+  // Redux state
+  // if we have a token we're logged in
+  const loggedIn = useSelector((state: RootState) => state.user.token);
+
+  // * redirect to '/' if not logged in
+  useEffect(() => {
+    if (!loggedIn) history.push("/");
+  }, [history, loggedIn]);
+
   return <div>Welcome home.</div>;
 }
