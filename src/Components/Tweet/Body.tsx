2a1bb3efe4e1d95ac164c560896c3cde16613fa0
Evan K <ekerik220@gmail.com>
2020-08-04 07:58:30 +0900

    Clicking on the parent tweet in a retweet sends user to comment page for parent tweet

--- src/Components/Tweet/Body.tsx
+++ src/Components/Tweet/Body.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import { RetweetBox } from "./RetweetBox";
+import { useHistory } from "react-router-dom";
 
 type PropTypes = { tweet: Tweet; parentTweet?: Tweet };
 
 export function Body(props: PropTypes) {
+  const history = useHistory();
+
+  // * Click handler. Redirects to comments page for parent tweet
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    history.push("/comments", props.parentTweet);
+  };
+
   return (
     <Container>
       <TweetBody>{props.tweet.body}</TweetBody>
-      <ParentBody>
+      <ParentBody onClick={handleClick}>
         {props.tweet.body && props.parentTweet ? (
           <RetweetBox tweet={props.parentTweet} />
         ) : (
